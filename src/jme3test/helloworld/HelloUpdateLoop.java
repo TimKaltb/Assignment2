d9e34387cb8f2384ee6352ee24f3cff010d25bcf
Tim <Tim@10.100.18.51>
2022-09-11 17:10:46 -0500

    HelloUpdateLoop Exercise Question 2

--- src/jme3test/helloworld/HelloUpdateLoop.java
+++ src/jme3test/helloworld/HelloUpdateLoop.java
@@ -16,29 +16,51 @@ public class HelloUpdateLoop extends SimpleApplication {
         app.start();
     }
 
-    protected Geometry player;
+    protected Geometry bluecube;
+    protected Geometry redcube;
 
     @Override
     public void simpleInitApp() {
         /** this blue box is our player character */
-        Box b = new Box(1, 1, 1);
-        player = new Geometry("blue cube", b);
-        Material mat = new Material(assetManager,
+        Box b1 = new Box(1, 1, 1);
+        bluecube = new Geometry("blue cube", b1);
+        Material mat1 = new Material(assetManager,
           "Common/MatDefs/Misc/Unshaded.j3md");
-        mat.setColor("Color", ColorRGBA.Red);
-        player.setMaterial(mat);
-        rootNode.attachChild(player);
+        mat1.setColor("Color", ColorRGBA.Blue);
+        bluecube.setMaterial(mat1);
+        rootNode.attachChild(bluecube);
+        
+        // create another box called b2
+        Box b2 = new Box(1, 1, 1);
+        redcube = new Geometry("red cube", b2);
+        // give the new box a colour
+        Material mat2 = new Material(assetManager,
+          "Common/MatDefs/Misc/Unshaded.j3md");
+        mat2.setColor("Color", ColorRGBA.Red);
+        redcube.setMaterial(mat2);
+        // move the new cube next to the first one
+        redcube.move(3, 0, 0);
+        // lets the new cube appear on the screen
+        rootNode.attachChild(redcube);
     }
 
     /* Use the main event loop to trigger repeating actions. */
     @Override
     public void simpleUpdate(float tpf) {
-        // make the player rotate:
-        player.rotate(0, -2*tpf, 0);
+        // make the blue cube rotate:
+        bluecube.rotate(0, 2*tpf, 0);
+        // make the red cube rotate twice as fast as the blue cube
+        redcube.rotate(0, 4*tpf, 0);
     }
     
     // 1.) What happens if you give the rotate() method negative numbers?
     
     // A: The cube will rotate in the opposite direction. It's rotating
     // to the left now, instead of to the right.
+    
+    
+    // 2.) Can you create two Geometries next to each other,
+    // and make one rotate twice as fast as the other?
+    
+    // The answer to this is documentated in the code above.
 }
